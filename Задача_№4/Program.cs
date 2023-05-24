8ac3dbf3cb70423fe20a3e3dad1b786eb2545221
Vitaly <kentanohin478@gmail.com>
2023-05-24 15:59:11 +0300

    Lessons 1

---
+++ Задача_№4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число");
+
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2 & n1 > n3)
+{
+Console.WriteLine($"max = {n1}");
+}
+else if (n2 > n1 & n2 > n3)
+{
+Console.WriteLine($"max = {n2}");
+}
+
+else if (n3 > n1 & n3 > n2)
+{
+Console.WriteLine($"max = {n3}");
+}
+else
+{
+Console.WriteLine("Числа равны между собой");
+}
+
---
+++ Задача_№6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Ведите число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+Console.WriteLine("Чётное число");
+}
+else
+{
+Console.WriteLine("Нечётное число");
+}
